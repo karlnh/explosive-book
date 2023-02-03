d574ced1f46a6f312b96d40029ca04c1fa5239d2
karlnh <hubigkarl@gmail.com>
2023-02-02 23:39:15 -0600

    set productTag constraints

--- models/ProductTag.js
+++ models/ProductTag.js
@@ -7,11 +7,18 @@ class ProductTag extends Model {}
 ProductTag.init(
   {
     // define columns
+    id: {
+      type: DataTypes.INTEGER,
+      allowNull: false,
+      primaryKey: true,
+      autoIncrement: true,
+    },
     product_id: {
       type: DataTypes.INTEGER,
-    },
-    tag_id: {
-      type: DataTypes.INTEGER,
+      references: {
+        model: 'product_tag',
+        key: 'id',
+      },
     }
   },
   {
