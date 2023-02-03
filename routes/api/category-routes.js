e5936be95827298d47d5e3bce8ffa2745029c22f
karlnh <hubigkarl@gmail.com>
2023-02-03 00:33:21 -0600

    create routes for objects

--- routes/api/category-routes.js
+++ routes/api/category-routes.js
@@ -6,23 +6,58 @@ const { Category, Product } = require('../../models');
 router.get('/', (req, res) => {
   // find all categories
   // be sure to include its associated Products
+  Category.findAll().then((data) => {
+    res.json(data);
+  });
 });
 
 router.get('/:id', (req, res) => {
   // find one category by its `id` value
   // be sure to include its associated Products
+  Category.findOne({
+    where: {
+      id: req.params.id,
+    },
+  }).then((data) => {
+    res.json(data);
+  });
 });
 
 router.post('/', (req, res) => {
   // create a new category
+  Category.create({
+    name: req.body.name
+  }).then(() => {
+    res.send("Successfully posted new category.");
+  })
+  
 });
 
 router.put('/:id', (req, res) => {
   // update a category by its `id` value
+  Category.update(
+    {
+      id: req.body.id,
+    },
+    {
+      where: {
+        id: req.params.id,
+      },
+    }
+  ).then((updatedCategory) => {
+    res.json(updatedCategory);
+  });
 });
 
 router.delete('/:id', (req, res) => {
   // delete a category by its `id` value
+  Category.destroy({
+    where: {
+      id: req.params.id,
+    },
+  }).then((deletedCategory) => {
+    res.json(deletedCategory);
+  });
 });
 
 module.exports = router;
--- routes/api/product-routes.js
+++ routes/api/product-routes.js
@@ -6,13 +6,28 @@ const { Product, Category, Tag, ProductTag } = require('../../models');
 // TODO: get all products
 router.get('/', (req, res) => {
   // find all products
-  // be sure to include its associated Category and Tag data
+  Product.findAll({
+    where: {
+        // be sure to include its associated Category and Tag data
+    }
+  }).then((data) => {
+    res.json(data);
+  });
 });
+
 
 // TODO: get one product
 router.get('/:id', (req, res) => {
   // find a single product by its `id`
-  // be sure to include its associated Category and Tag data
+  Product.findOne({
+    where: {
+      id: req.params.id,
+      // be sure to include its associated Category and Tag data
+    }
+  }).then((data) => {
+    res.json(data);
+  });
+
 });
 
 // TODO: create new product
@@ -25,7 +40,12 @@ router.post('/', (req, res) => {
       tagIds: [1, 2, 3, 4]
     }
   */
-  Product.create(req.body)
+  Product.create({
+    product_name: req.body.product_name,
+    price: req.body.price,
+    stock: req.body.stock,
+    tagIds: req.body.tag_id // ???????????????????????????
+  })
     .then((product) => {
       // if there's product tags, we need to create pairings to bulk create in the ProductTag model
       if (req.body.tagIds.length) {
@@ -91,6 +111,13 @@ router.put('/:id', (req, res) => {
 
 router.delete('/:id', (req, res) => {
   // delete one product by its `id` value
+  Product.destroy({
+    where: {
+      id: req.params.id
+    }
+  }).then((deletedProduct) => {
+    res.json(deletedProduct);
+  })
 });
 
 module.exports = router;
